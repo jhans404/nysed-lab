3a94b214f29395ec2c6fc452a86195ab881e3503
Joshua Hans <jhans@fdrhs.org>
2020-10-17 16:02:36 +0000

    added 3 cards

--- src/components/SchoolCard.js
+++ src/components/SchoolCard.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './css/SchoolCard.css';
 
-function SchoolCard() {
+function SchoolCard(props) {
   return(
     <div className="col-4">
       <div className="card">
-        <h5 className="card-header">PASS SCHOOL NAME HERE</h5>
+        <h5 className="card-header">{props.school.ENTITY_NAME}</h5>
         <div className="card-body">
           <h5 className="card-title">
             <span className="badge">2017-18</span>
@@ -14,9 +14,9 @@ function SchoolCard() {
           </h5>
           <span className="badge badge-header">Student Data</span>
           <ul className="list-group">
-            <li className="list-group-item">Enrollment Data</li>
-            <li className="list-group-item">3-8 ELA Assessment Data</li>
-            <li className="list-group-item">3-8 Math Assessment Data</li>
+            <li className="list-group-item">% Male: {props.school.PER_MALE}, % Female: {props.school.PER_FEMALE}</li>
+            <li className="list-group-item">% ELL: {props.school.PER_ELL}</li>
+            <li className="list-group-item">% SWD: {props.school.PER_SWD}</li>
           </ul>
           <span className="badge badge-header">School Data</span>
           <ul className="list-group">
